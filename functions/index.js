899d1ebcfd0996b9c7cb53a5e18ea1adfd9bfede
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-03-19 22:24:45 +1100

    Switch to dialogflow // add coffee intent responds with user input

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const { actionssdk } = require('actions-on-google');
+const { dialogflow } = require('actions-on-google');
 const functions = require('firebase-functions');
 
-const app = actionssdk({
+const app = dialogflow({
   debug: true
 });
 
-app.intent('actions.intent.MAIN', (conv) => {
-  conv.ask('Hi!');
-});
+app.intent('Add Coffee', (conv, { coffees }) => {
+  conv.close(`We are adding ${coffees}. See you next time!`)
+})
 
 // More intent handling if needed
 exports.lifeStyleFullfillment = functions.https.onRequest(app);

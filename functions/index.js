11f5a5bd9111f620ce5778e9ed3ca94f42d5ab09
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-03-20 20:41:32 +1100

    Connect to realtime firebase and store the coffee count on request

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,28 @@
 
 const { dialogflow } = require('actions-on-google');
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require("./life-stats-168c9-firebase-adminsdk-tiaoj-4c55fe4206.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://life-stats-168c9.firebaseio.com"
+});
 
 const app = dialogflow({
   debug: true
 });
 
 app.intent('Add Coffee', (conv, { coffees }) => {
-  conv.close(`We are adding ${coffees}. See you next time!`)
-})
+  conv.ask(`Processing your request...`);
+  return admin.database().ref('/coffee').push({ coffee: coffees })
+    .then((snapshot) => {
+      return conv.close(`We've added ${coffees} coffees`);
+    })
+    .catch((e) => {
+      return conv.ask(`Something went wrong. Try adding your coffees again`)
+    });
+});
 
 // More intent handling if needed
 exports.lifeStyleFullfillment = functions.https.onRequest(app);
